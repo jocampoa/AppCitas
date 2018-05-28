7fcfe218755d8da04f1fcaa50e2e0a57a065d46a
Juan Ocampo <david.ocampo.3928@gmail.com>
2018-05-27 21:50:40 -0500

    Users in Backend

---
+++ AppCitas.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,11 @@
+﻿namespace AppCitas.Backend.Models
+{
+    using Domain;
+
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<AppCitas.Domain.User> Users { get; set; }
+
+        public System.Data.Entity.DbSet<AppCitas.Domain.UserType> UserTypes { get; set; }
+    }
+}
